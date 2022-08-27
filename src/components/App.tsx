7aae7b6d86e7ea1dc5b4c7544f0555985e51f047
Filipe Sabella <me@filipesabella.com>
2022-08-27 17:30:37 -0300

    Clean up

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -23,12 +23,12 @@ export function App(): JSX.Element {
     database.initialize().then(_ => setLoading(false));
 
     // load a song on start for debugging
-    database.listCurrentSongs().then(songs => {
-      // setCurrentSong(testSong);
-      // database.song(songs[0].id).then(s => {
-      //   setCurrentSong(s!);
-      // });
-    });
+    const songIndex = window.location.search.split('?')[1]?.split('=')[1];
+    if (songIndex) {
+      database.listCurrentSongs().then(songs => {
+        setCurrentSong(songs[parseInt(songIndex)]);
+      });
+    }
   }, []);
 
 
@@ -47,31 +47,3 @@ export function App(): JSX.Element {
 
   return app;
 }
-
-const notes = 'C C# D D# E F F# G G# A A# B'.split(' ');
-const octaves = [/*0, 1, 2, */3, 4, 5, 6, 7, 8];
-
-const testSong: Song = {
-  id: '111',
-  name: 'henlo',
-  notes: octaves.flatMap(o => notes.map((n, ni) => ({
-    id: `${n}${o}`,
-    name: `${n}${o}`,
-    midi: 24 + o * 12 + ni,
-    preferredEwiFingering: null,
-  }))),
-  bookmarks: [],
-};
-
-
-const testSong2: Song = {
-  id: '111',
-  name: 'henlo',
-  notes: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => ({
-    id: `${i}`,
-    name: 'A#4',
-    midi: 58,
-    preferredEwiFingering: null,
-  })),
-  bookmarks: [],
-};
